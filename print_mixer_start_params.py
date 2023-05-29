a55a5e1327e7e732809e6c5cecff45113e136397
Stanislav Zhdanovich <st.zhdanovich@gmail.com>
2023-05-29 16:05:18 +0700

    add protocol params printer

--- print_mixer_start_params.py
+++ print_mixer_start_params.py
@@ -1,6 +1,7 @@
 from dotenv import load_dotenv
-from pycardano import Network, PaymentVerificationKey, Address
 import os
+from pycardano import (Network, PaymentVerificationKey)
+from mixer import OwnerScript
 from chain import ChainQuery
 
 load_dotenv()
@@ -12,5 +13,9 @@ context = ChainQuery(os.environ['BLOCKFROST_PROJECT_ID'],
 
 owner_verification_key = PaymentVerificationKey.load("owner.vkey")
 owner_pub_key_hash = owner_verification_key.hash()
-owner_address = Address(
-    payment_part=owner_pub_key_hash, network=network)
+
+mixer_owner = OwnerScript(network, context, owner_verification_key)
+# Mixer Creator: d1ce83174feeb6ae11d95fd47cac403642cb616b244dbb32a2ca0bda
+# Script start slot: 29667384
+# Mixer NFT currency symbol: bbd65a4af3dd5bb07b11cfb66418cdffc6bd26817559e0c5a80f405d
+mixer_owner.print_start_params()
