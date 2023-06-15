b4c72cbbee42c51b2ac7a0710b8796511179569c
stanislav-az <st.zhdanovich@gmail.com>
2023-06-15 13:27:41 +0700

    add phased deploy

--- mixer/start.py
+++ mixer/start.py
@@ -1,8 +1,9 @@
-from typing import List
-from pycardano import (Network, Address, PaymentVerificationKey, PaymentSigningKey,
+from typing import List, Tuple
+from pycardano import (Network, Address, Redeemer, PlutusV1Script, PaymentVerificationKey, PaymentSigningKey, ScriptHash,
                        TransactionOutput, TransactionBuilder, Value, datum_hash, MultiAsset, PlutusV2Script, plutus_script_hash)
+from pycardano.serialization import IndefiniteList
 from chain import ChainQuery, SubmitTx
-from .datums import *
+from .datums import DepositDatum, Vault, WithdrawDatum, Unit
 from .owner_script import OwnerScript
 
 
@@ -14,23 +15,101 @@ class MixerStart(SubmitTx):
                  context: ChainQuery,
                  signing_key: PaymentSigningKey,
                  verification_key: PaymentVerificationKey,
-                 mixer_script: PlutusV2Script,
-                 script_start_slot: int,
+
                  ) -> None:
         super().__init__(network, context, signing_key, verification_key)
-        self.mixer_script = mixer_script
-        self.mixer_script_hash = plutus_script_hash(self.mixer_script)
-        self.mixer_address = Address(
-            payment_part=self.mixer_script_hash, network=self.network)
-        self.script_start_slot = script_start_slot
-        self.mixer_datum = MixerDatum(nullifierHashes=IndefiniteList([]))
 
-    def start_mixer(self):
-        # Create a locking script that hold mixer script and also mints mixer NFT
-        mixer_owner = OwnerScript(
-            self.network, self.context, self.verification_key)
-        owner_script = mixer_owner.mk_owner_script(self.script_start_slot)
-        owner_script_hash = owner_script.hash()
+    def start_mixer(self, owner_utxo_ref: Tuple[str, int], plutus_minting_script: PlutusV1Script,
+                    deposit_script_hash: ScriptHash, withdraw_script_hash: ScriptHash, deposit_tree_datum: DepositDatum):
+        # Create a script that mints mixer NFT
+        owner = OwnerScript(self.network, self.context,
+                            self.signing_key, self.verification_key)
+        utxo = owner.find_utxo(owner_utxo_ref)
+        policy = plutus_minting_script
+        policy_id = plutus_script_hash(policy)
+        print(f"Protocol token Policy id: {policy_id.payload.hex()}")
+
+        # Create a transaction builder
+        builder = TransactionBuilder(self.context)
+
+        # Add our own address as the input address
+        builder.add_input(utxo)
+        builder.add_input_address(self.address)
+
+        ############ Mixer NFT minting: ############
+        deposit_tree_token = MultiAsset.from_primitive(
+            {
+                policy_id.payload: {
+                    b"Deposit Tree Token": 1,
+                }
+            }
+        )
+        vault_token = MultiAsset.from_primitive(
+            {
+                policy_id.payload: {
+                    b"Vault Token": 1,
+                }
+            }
+        )
+        nullifier_store_token = MultiAsset.from_primitive(
+            {
+                policy_id.payload: {
+                    b"Nullifier Store Token": 1,
+                }
+            }
+        )
+        mixer_nfts = deposit_tree_token + vault_token + nullifier_store_token
+
+        # Set nft we want to mint
+        builder.mint = mixer_nfts
+
+        # Set plutus mint script
+        builder.add_minting_script(
+            script=plutus_minting_script, redeemer=Redeemer(data=Unit()))
+
+        ############ Mixer first outputs creation: ############
+        deposit_script_address = Address(
+            payment_part=deposit_script_hash, network=self.network)
+        print(
+            f"Deposit script hash: {deposit_script_hash} \nDeposit script address: {deposit_script_address}")
+
+        withdraw_script_address = Address(
+            payment_part=withdraw_script_hash, network=self.network)
+        print(
+            f"Withdraw script hash: {withdraw_script_hash} \nWithdraw script address: {withdraw_script_address}")
+
+        # Deposit Tree
+        print(f"Deposit Tree datum hash: {datum_hash(deposit_tree_datum)}")
+        deposit_tree_output = TransactionOutput(datum=deposit_tree_datum,
+                                                address=deposit_script_address, amount=Value(2000000, deposit_tree_token))
+        builder.add_output(deposit_tree_output)
+
+        # Vault
+        vault_datum = Vault()
+        print(f"Vault datum hash: {datum_hash(vault_datum)}")
+        vault_output = TransactionOutput(datum=vault_datum,
+                                         address=deposit_script_address, amount=Value(2000000, vault_token))
+        builder.add_output(vault_output)
+
+        # Nullifier Store
+        nullifier_store_datum = WithdrawDatum(
+            nullifierHashes=IndefiniteList([]))
+        print(
+            f"Nullifier Store datum hash: {datum_hash(nullifier_store_datum)}")
+        nullifier_store_output = TransactionOutput(datum=nullifier_store_datum,
+                                                   address=withdraw_script_address, amount=Value(2000000, nullifier_store_token))
+        builder.add_output(nullifier_store_output)
+
+        builder.required_signers = [self.pub_key_hash]
+        self.submit_tx_builder(builder)
+
+    def deploy_deposit_script(self, deposit_script: PlutusV2Script):
+        ############ Reference script creation: ############
+        deposit_script_hash = plutus_script_hash(deposit_script)
+        deposit_script_address = Address(
+            payment_part=deposit_script_hash, network=self.network)
+        print(
+            f"Deposit script hash: {deposit_script_hash} \nDeposit script address: {deposit_script_address}")
 
         # Create a transaction builder
         builder = TransactionBuilder(self.context)
@@ -38,51 +117,42 @@ class MixerStart(SubmitTx):
         # Add our own address as the input address
         builder.add_input_address(self.address)
 
-        # Required since owner script is InvalidBefore type
-        builder.validity_start = self.script_start_slot
-
-        ############ Reference script creation: ############
-        owner_script_addr = Address(
-            payment_part=owner_script_hash, network=self.network)
-
-        print(f"Locking script address: {owner_script_addr}")
-
         # Reference script output
         reference_script_utxo = TransactionOutput(
-            address=owner_script_addr,
+            address=deposit_script_address,
             amount=25000000,
-            script=self.mixer_script
+            script=deposit_script
         )
 
         # Add reference script
         builder.add_output(reference_script_utxo)
 
-        ############ Mixer NFT minting: ############
-        mixer_nft = MultiAsset.from_primitive(
-            {
-                owner_script_hash.payload: {
-                    b'Mixer Protocol Token': 1,  # Name of our token  # Quantity of this token
-                }
-            }
+        builder.required_signers = [self.pub_key_hash]
+        self.submit_tx_builder(builder)
+
+    def deploy_withdraw_script(self, withdraw_script: PlutusV2Script):
+        ############ Reference script creation: ############
+        withdraw_script_hash = plutus_script_hash(withdraw_script)
+        withdraw_script_address = Address(
+            payment_part=withdraw_script_hash, network=self.network)
+        print(
+            f"Withdraw script hash: {withdraw_script_hash} \nWithdraw script address: {withdraw_script_address}")
+
+        # Create a transaction builder
+        builder = TransactionBuilder(self.context)
+
+        # Add our own address as the input address
+        builder.add_input_address(self.address)
+
+        # Reference script output
+        reference_script_utxo = TransactionOutput(
+            address=withdraw_script_address,
+            amount=25000000,
+            script=withdraw_script
         )
 
-        print(
-            f"Protocol token currency / Policy id: {owner_script_hash.payload.hex()}")
+        # Add reference script
+        builder.add_output(reference_script_utxo)
 
-        # Set nft we want to mint
-        builder.mint = mixer_nft
-
-        # Set native script
-        builder.native_scripts = [owner_script]
-
-        ############ Mixer first output creation: ############
-        print(
-            f"Mixer script hash: {self.mixer_script_hash} \nMixer script address: {self.mixer_address}")
-
-        print(f"Mixer datum hash: {datum_hash(self.mixer_datum)}")
-
-        output_with_inline_datum = TransactionOutput(datum=self.mixer_datum,
-                                                     address=self.mixer_address, amount=Value(2000000, mixer_nft))
-        builder.add_output(output_with_inline_datum)
-
+        builder.required_signers = [self.pub_key_hash]
         self.submit_tx_builder(builder)
