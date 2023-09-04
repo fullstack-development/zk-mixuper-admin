da45a8a5b1fc351720876db7ee0db30c298eed43
stanislav-az <st.zhdanovich@gmail.com>
2023-09-04 21:04:04 +0700

    add staged tx submission

--- chain/query.py
+++ chain/query.py
@@ -1,4 +1,10 @@
-from pycardano import BlockFrostChainContext, Network, TransactionBuilder, TransactionOutput, Transaction
+from pycardano import (
+    BlockFrostChainContext,
+    Network,
+    TransactionBuilder,
+    TransactionOutput,
+    Transaction,
+)
 from blockfrost import ApiUrls, BlockFrostApi
 from blockfrost.utils import request_wrapper
 import os
@@ -7,13 +13,16 @@ from retry import retry
 
 
 class BlockFrostDatumApi(BlockFrostApi):
-
-    def __init__(self, project_id: str = None, base_url: str = None, api_version: str = None):
+    def __init__(
+        self, project_id: str = None, base_url: str = None, api_version: str = None
+    ):
         super().__init__(
             project_id=project_id,
-            base_url=base_url if base_url else os.environ.get(
-                'BLOCKFROST_API_URL', default=ApiUrls.mainnet.value),
-            api_version=api_version)
+            base_url=base_url
+            if base_url
+            else os.environ.get("BLOCKFROST_API_URL", default=ApiUrls.mainnet.value),
+            api_version=api_version,
+        )
 
     @request_wrapper
     def script_datum_cbor(self, datum_hash: str, **kwargs):
@@ -33,26 +42,26 @@ class BlockFrostDatumApi(BlockFrostApi):
         """
         return requests.get(
             url=f"{self.url}/scripts/datum/{datum_hash}/cbor",
-            headers=self.default_headers
+            headers=self.default_headers,
         )
+
+
+class CollateralException(Exception):
+    pass
 
 
 class ChainQuery(BlockFrostChainContext):
     def __init__(
         self, project_id: str, network: Network = Network.TESTNET, base_url: str = None
     ):
-        super().__init__(
-            project_id=project_id,
-            network=network,
-            base_url=base_url
-        )
+        super().__init__(project_id=project_id, network=network, base_url=base_url)
         self.api = BlockFrostDatumApi(
-            project_id=self._project_id, base_url=self._base_url)
+            project_id=self._project_id, base_url=self._base_url
+        )
 
     def _get_datum(self, utxo):
         if utxo.output.datum_hash is not None:
-            datum = self.api.script_datum_cbor(
-                str(utxo.output.datum_hash)).cbor
+            datum = self.api.script_datum_cbor(str(utxo.output.datum_hash)).cbor
             return datum
         return None
 
@@ -68,8 +77,7 @@ class ChainQuery(BlockFrostChainContext):
     @retry(delay=10)
     def wait_for_tx(self, tx_id):
         self.api.transaction(tx_id)
-        print(
-            f"Transaction {tx_id} has been successfully included in the blockchain.")
+        print(f"Transaction {tx_id} has been successfully included in the blockchain.")
 
     def submit_tx_with_print(self, tx: Transaction):
         print("############### Transaction created ###############")
@@ -85,14 +93,14 @@ class ChainQuery(BlockFrostChainContext):
         for utxo in self.utxos(str(target_address)):
             if utxo.output.amount.coin >= 10000000:
                 return utxo
-        return None
+        raise CollateralException("Please create collateral utxo manually")
 
     def create_collateral(self, target_address, skey):
         collateral_builder = TransactionBuilder(self)
 
         collateral_builder.add_input_address(target_address)
-        collateral_builder.add_output(
-            TransactionOutput(target_address, 10000000))
+        collateral_builder.add_output(TransactionOutput(target_address, 10000000))
 
         self.submit_tx_with_print(
-            collateral_builder.build_and_sign([skey], target_address))
+            collateral_builder.build_and_sign([skey], target_address)
+        )
--- chain/tx.py
+++ chain/tx.py
@@ -1,24 +1,30 @@
 """offchain code for tx handling"""
-from pycardano import (Network, Address, PaymentVerificationKey, PaymentSigningKey,
-                       TransactionBuilder)
+from pycardano import (
+    Network,
+    Address,
+    PaymentVerificationKey,
+    PaymentSigningKey,
+    TransactionBuilder,
+    Transaction,
+    VerificationKeyWitness,
+)
 from .query import ChainQuery
 
 
-class SubmitTx():
-
-    def __init__(self,
-                 network: Network,
-                 context: ChainQuery,
-                 signing_key: PaymentSigningKey,
-                 verification_key: PaymentVerificationKey,
-                 ) -> None:
+class SubmitTx:
+    def __init__(
+        self,
+        network: Network,
+        context: ChainQuery,
+        signing_key: PaymentSigningKey,
+        verification_key: PaymentVerificationKey,
+    ) -> None:
         self.network = network
         self.context = context
         self.signing_key = signing_key
         self.verification_key = verification_key
         self.pub_key_hash = self.verification_key.hash()
-        self.address = Address(
-            payment_part=self.pub_key_hash, network=self.network)
+        self.address = Address(payment_part=self.pub_key_hash, network=self.network)
 
     def submit_tx_builder(self, builder: TransactionBuilder):
         """adds collateral and signers to tx , sign and submit tx."""
@@ -28,5 +34,31 @@ class SubmitTx():
         builder.required_signers = [self.pub_key_hash]
 
         signed_tx = builder.build_and_sign(
-            [self.signing_key], change_address=self.address, collateral_change_address=self.address)
+            [self.signing_key],
+            change_address=self.address,
+            collateral_change_address=self.address,
+        )
         self.context.submit_tx_with_print(signed_tx)
+
+    def build_tx(self, builder: TransactionBuilder) -> Transaction:
+        collateral_utxo = self.context.find_collateral(self.address)
+
+        builder.collaterals.append(collateral_utxo)
+
+        tx_body = builder.build(
+            change_address=self.address, collateral_change_address=self.address
+        )
+        witness_set = builder.build_witness_set()
+        witness_set.vkey_witnesses = []
+
+        return Transaction(tx_body, witness_set, auxiliary_data=builder.auxiliary_data)
+
+    def sign_tx(self, tx: Transaction):
+        signature = self.signing_key.sign(tx.transaction_body.hash())
+        tx.transaction_witness_set.vkey_witnesses.append(
+            VerificationKeyWitness(self.verification_key, signature)
+        )
+
+    def sign_and_submit_tx(self, tx: Transaction):
+        self.sign_tx(tx)
+        self.context.submit_tx_with_print(tx)
